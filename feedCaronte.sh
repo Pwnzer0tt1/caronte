0bde2fcfed00f573421baf79b4ba3dff09e9153f
Domingo Dirutigliano <me@domy.sh>
2023-07-22 13:55:22 +0200

    feedCaronte Update

--- feedCaronte.sh
+++ feedCaronte.sh
@@ -3,16 +3,19 @@
 #	.		TIMEOUT    CARONTE_IP
 #./caronte.sh 30 https://caronte.com game
 
-if [[ "$#" -ne 3 ]]; then
+if [[ "$#" -lt 3 ]]; then
 	echo "Usage: $0 <timeout> <ip:port> <interface>"
 	exit 2
 fi
 
-PROC_TIMEOUT=$$
+CURRENT_DIRECTORY=$(realpath $(dirname "$0"))
+TMPFILE_SCRIPT="/tmp/caronte_upload_move_script_tmp.sh"
+cd $CURRENT_DIRECTORY
+
 UPLOAD_PROC=$$
 THIS_PROC=$$
 
-trap 'echo; kill -9 $PROC_TIMEOUT; kill -9 $UPLOAD_PROC; kill -9 $THIS_PROC' INT
+trap 'echo; kill -9 $UPLOAD_PROC; kill -9 $THIS_PROC' INT
 
 TIMEOUT_TCPDUMP="$1"
 CARONTE_ADDR="$2"
@@ -20,20 +23,18 @@ INTERFACE_NAME="$3"
 
 mkdir upload 2> /dev/null
 
-function get_pcaps {
-  while true; do
-	  timeout $TIMEOUT_TCPDUMP tcpdump -w "data.pcap" -i $INTERFACE_NAME port not 22 and port not 4444 &> /dev/null & PROC_TIMEOUT=$!
-    wait $PROC_TIMEOUT
-    mv data.pcap "upload/data-$(md5sum <<< date | awk '{ print $1 }').pcap"
-  done
-}
+echo "#!/usr/bin/env bash
+cd $CURRENT_DIRECTORY
+mv \$1 upload/\$1 
+" > $TMPFILE_SCRIPT
+chmod +x $TMPFILE_SCRIPT
 
 function upload_pcaps { 
   while true; do
     files=`ls ./upload/*.pcap 2> /dev/null`
     for file in $files
     do
-      curl -F "file=@$file" -F "flush_all=true" "$CARONTE_ADDR/api/pcap/upload" && rm $file
+      curl -F "file=@$file" -F "flush_all=false" "$CARONTE_ADDR/api/pcap/upload" && rm $file
       echo
     done
     sleep `echo $TIMEOUT_TCPDUMP/2 | bc`
@@ -42,5 +43,6 @@ function upload_pcaps {
 
 upload_pcaps & UPLOAD_PROC=$!
 
-get_pcaps
+args=($@)
+tcpdump -G $TIMEOUT_TCPDUMP -z $TMPFILE_SCRIPT -w capture-%Y%m%d-%H%M%S.pcap -i $INTERFACE_NAME ${args[@]:4:$(echo "$#")} 
 
